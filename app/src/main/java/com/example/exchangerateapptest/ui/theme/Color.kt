016b2f1919d9e5c03ac3a1086d1d667eebc34b7e
Raman Martsulevich <roman.marc.dev@gmail.com>
2025-03-11 22:34:10 +0100

    refactor: add resources into the project

--- app/src/main/java/com/example/exchangerateapptest/ui/theme/Color.kt
+++ app/src/main/java/com/example/exchangerateapptest/ui/theme/Color.kt
@@ -1,6 +1,8 @@
 package com.example.exchangerateapptest.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import androidx.room.PrimaryKey
+import kotlin.reflect.jvm.internal.impl.descriptors.Visibilities.Private
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -9,3 +11,17 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PrimaryKey = Color(0xFF0A2FA7)
+val LightPrimary = Color(0xFFFFFFFF)
+
+val On_primary = Color(0xFFDEE4F8)
+val Secondary= Color(0xFF9DACDC)
+val TextDefault = Color(0xFF343138)
+val TextSecondary = Color(0xFF767676)
+val Outline = Color(0xFFEBEBEB)
+val Yellow= Color(0xFFFFB800)
+
+val Bg_default= Color(0xFFDEE4F8)
+val Bg_header= Color(0xFFF6F6F6)
+val Bg_head= Color(0xFFF0F2F8)
--- app/src/main/java/com/example/exchangerateapptest/ui/theme/Type.kt
+++ app/src/main/java/com/example/exchangerateapptest/ui/theme/Type.kt
@@ -2,33 +2,55 @@ package com.example.exchangerateapptest.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.exchangerateapptest.R
+
+val InterFont = FontFamily(
+	Font(R.font.inter_medium, FontWeight.Medium),
+	Font(R.font.inter_semibold, FontWeight.SemiBold),
+	Font(R.font.inter_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-	bodyLarge = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.Normal,
-		fontSize = 16.sp,
-		lineHeight = 24.sp,
-		letterSpacing = 0.5.sp
-	)
-	/* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
+	headlineLarge = TextStyle(
+		fontFamily = InterFont,
+		fontWeight = FontWeight.Bold,
+		fontSize = 22.sp,
+		lineHeight = 28.sp,
+		letterSpacing = 0.sp
+	),
+    bodySmall = TextStyle(
+        fontFamily = InterFont,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+	bodyMedium = TextStyle(
+		fontFamily = InterFont,
+		fontWeight = FontWeight.Medium,
+		fontSize = 16.sp,
+		lineHeight = 20.sp,
+		letterSpacing = 0.sp
+	),
+    bodyLarge = TextStyle(
+        fontFamily = InterFont,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp
+    ),
+	labelSmall = TextStyle(
+		fontFamily = InterFont,
+		fontWeight = FontWeight.SemiBold,
+		fontSize = 12.sp,
+		lineHeight = 16.sp,
+		letterSpacing = 0.sp
+	)
+
+
 )
