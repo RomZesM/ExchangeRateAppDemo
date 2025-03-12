9fe679d2555e07af0a7c2813338dd5da55d96a69
Raman Martsulevich <roman.marc.dev@gmail.com>
2025-03-12 17:33:42 +0100

    feature: add design for TopBar

--- app/src/main/java/com/example/exchangerateapptest/screens/main/MyAppTopBar.kt
+++ app/src/main/java/com/example/exchangerateapptest/screens/main/MyAppTopBar.kt
@@ -1,23 +1,28 @@
 package com.example.exchangerateapptest.screens.main
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.exchangerateapptest.R
+import com.example.exchangerateapptest.ui.theme.LocalExtendedColors
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
 fun MyTopAppBar(
 	navController: NavHostController
@@ -36,24 +41,73 @@ fun MyTopAppBar(
 		Screen.Filters.route
 	)
 
-	CenterAlignedTopAppBar(
+
+	MyTopAppBarLayout(
+		currentRoute = currentRoute,
+		currentRouteTitle = currentRouteTitle,
+		onBackIconCLick = { navController.popBackStack() },
+		listOfScreensWithBackArrow = listOfScreensWithBackArrow
+	)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTopAppBarLayout(
+	currentRoute: String,
+	currentRouteTitle: String,
+	onBackIconCLick: () -> Unit,
+	listOfScreensWithBackArrow: List<String>
+) {
+
+	val extendedColors = LocalExtendedColors.current
+
+	TopAppBar(
 		title = {
 			Row(
-				verticalAlignment = Alignment.CenterVertically
+				modifier = Modifier
+					.fillMaxWidth(),
+
+				verticalAlignment = Alignment.CenterVertically,
+				horizontalArrangement = Arrangement.Start
 			) {
 				if (currentRoute in listOfScreensWithBackArrow) {
 					IconButton(
-						onClick = { navController.popBackStack() },
-						content = { Icon(Icons.AutoMirrored.Filled.ArrowBack, null) }
+						onClick = { onBackIconCLick() },
+						content = {
+							Icon(
+								painter = painterResource(
+									R.drawable.arrow_back_icon
+								),
+								contentDescription = "Dropdown Icon",
+								tint = extendedColors.primary
+							)
+						}
 					)
 				}
 
 				Text(
 					text = currentRouteTitle,
-					color = Color.White
+					color = extendedColors.textDefault,
+					style = MaterialTheme.typography.headlineLarge,
+					modifier = Modifier.padding(start = 4.dp)
+
 				)
 			}
 		},
-		colors = TopAppBarDefaults.topAppBarColors(MaterialTheme.colorScheme.primary),
+		colors = TopAppBarDefaults.topAppBarColors(extendedColors.bgHeader),
+	)
+
+}
+
+@Preview
+@Composable
+fun MyTopAppBarPreview() {
+	MyTopAppBarLayout(
+		currentRoute = Screen.Filters.route,
+		currentRouteTitle = Screen.Filters.route,
+		onBackIconCLick = {},
+		listOfScreensWithBackArrow = listOf(
+			Screen.Filters.route
+		)
 	)
 }
